c4b260bc0bed1c1628323c1c05ea2d14808c2169
Nadeemshahg <160504583+Nadeemshahg@users.noreply.github.com>
2024-03-26 21:10:40 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,83 @@
+// //  Question No:2  Personal Message: Store a person’s name in a variable,
+// //   and print a message to that person. Your message should be simple,
+// //    such as, “Hello nadeem, Can I help You?”
+
+// // let $name:string='nadeem shah'
+// // console.log(`hello ${$name} can i help you`)
+
+
+// //  Question No:3 Name Cases: Store a person’s name in a variable, 
+// //   and then print that person’s name in lowercase, uppercase,
+// //    and titlecase.
+
+// // let person_name: string='nadeem '
+// // console.log(person_name.toLowerCase())
+// // console.log(person_name.toUpperCase())
+// // console.log(person_name.charAt(0).toUpperCase()+person_name.slice(1));
+
+// // Question No:4  Famous Quote: Find a quote from a famous person you admire. 
+// // Print the quote and the name of its author. 
+// // Your output should look something like the following, including
+// //  the quotation marks:
+
+// // console.log(`nadeem one said,"setting goals is the first step in turning  the invisible into the visible"`);
+
+// // Question No:5 Famous Quote 2: Repeat Exercise 4, but this time store 
+// // the famous person’s name in a variable called famous_person. 
+// // Then compose your message and store it in a new variable called message.
+// //  Print your message.
+
+// // let famous_person='nadeem'
+// // let quote='setting goals is the first step in turning  the invisible into the visible'
+// // console.log(`${famous_person} once said,${quote}`);
+
+// // Question No:7&8  Number Eight: Write addition, subtraction, multiplication, 
+// // and division operations that each result in the number 8. Be sure to enclose your operations 
+// // in print statements to see the results.
+
+// // console.log(5+3);
+// // console.log(11-3);
+// // console.log(4*2);
+// // console.log(16/2);
+
+
+// //Question No:9 Favorite Number: Store your favorite number in a variable.
+// //  Then, using that variable, create a message that reveals
+// //   your favorite number. Print that message.
+
+
+// // let favoriteNumber: number=786
+// // console.log(`my favorite Number is ${favoriteNumber}`);
+
+
+// // Question  No:10 Adding Comments: Choose two of the programs you’ve 
+// // written, and add at least one comment to each. If you don’t have 
+// // anything specific to write because your programs are too simple at this
+// //  point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+// console.log('hello I am  I.T Student');
+// const $name:string='nadeem shah'
+// const Age:number=27
+// const educaton:string[]=['MA.BA.M.ed, B.ed.(in me se kuch bhi nahe kia)']
+// const skill:string[]=['typescript',"javascript"]
+// const experience: string="1 monthe"
+// const project:string[]=['web designe','game designe','app designe','']
+// console.log(`my name is ${$name},and i am (${Age}) year old`);
+// console.log(`Educational Degre ${educaton}`);
+// console.log('I have Skills in');
+// skill.forEach(skill=> console.log('-'+skill))
+// console.log("Abhi sekh raha hun");
+// console.log(`i have (${experience}) experience in vs coding`);
+// console.log('Mere Sapny hen ye');
+// project.forEach(project=>console.log(' * '+project));
+
+
+
+
+
+
+
+
+
+
